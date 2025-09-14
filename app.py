39d01ad7d4a2826bea064892bd731306abbfcb2e
joshua Cheng <chengjoshua22@gmail.com>
2025-09-13 21:48:05 -0500

    done setting up the button for the google oauth

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template, jsonify, request;
+from dotenv import load_dotenv
+from google.oauth2 import id_token
+from google.auth.transport import requests as google_oauth_requests
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+google_client_id = os.environ.get('GOOGLE_CLIENT_ID')
 
 app = Flask(__name__)
 
@@ -27,7 +36,58 @@ def home_page():
 
 @app.route('/signup')
 def signup():
-    return render_template("signup.html")
+    return render_template("signup.html", GOOGLE_CLIENT_ID=google_client_id) # its fine to send the id to the front end. its just a public identifier for your app to use google auth
+
+@app.route('/auth/google', methods=['POST'])
+def google_auth():
+    print("Google auth endpoint hit: /auth/google")
+
+    data = request.get_json()
+    # print(f"data: {data}")
+    
+    if not data or data["token"] == None:
+        return jsonify({"success": False, "message": "No token provided"}), 400
+    
+    token = data['token']
+
+    try:
+        print("Verifying token...")
+        idinfo = id_token.verify_oauth2_token(
+            token, 
+            google_oauth_requests.Request(), 
+            google_client_id
+        )
+        print(f"idinfo: {idinfo}")
+        
+        user_id = idinfo['sub']
+        email = idinfo['email']
+        name = idinfo.get('name', '')
+        
+        print(f"Authenticated user: {email}")
+
+        # TODO: Check if user exists in database and decide signup or login
+        # returning account info from DB upon successful auth
+        return jsonify({
+            "success": True, 
+            "message": "Authentication successful",
+            "user": {
+                "id": user_id,
+                "email": email,
+                "name": name
+            }
+        })
+        
+    except ValueError as e:
+        print(f"Error verifying token: {e}")
+        return jsonify({"success": False, "message": f"auth failed"}), 401
+
+    
+    return jsonify({"success": True, "message": "just testing for now"})
+
+@app.after_request
+def add_security_headers(response):
+    response.headers['Cross-Origin-Opener-Policy'] = 'same-origin-allow-popups'
+    return response
 
 app.static_folder = "static"
 
